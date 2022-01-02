9963ef724835aec5794d52d41aef8d4c9aa3ad72
Yauheni Tarasevich <evgeniy123@gmail.com>
2022-01-02 20:17:09 +0100

    first commit

---
+++ lib/CrEOF/Spatial/DBAL/Platform/MySql.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright (C) 2015 Derek J. Lambert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CrEOF\Spatial\DBAL\Platform;
+
+use CrEOF\Spatial\DBAL\Types\AbstractSpatialType;
+use CrEOF\Spatial\PHP\Types\Geography\GeographyInterface;
+use CrEOF\Spatial\DBAL\Types\GeographyType;
+
+/**
+ * MySql spatial platform
+ *
+ * @author  Derek J. Lambert <dlambert@dereklambert.com>
+ * @license http://dlambert.mit-license.org MIT
+ */
+class MySql extends AbstractPlatform
+{
+    /**
+     * For Geographic types MySQL follows the WKT specifications and returns (latitude,longitude) while (x,y) / (longitude,latitude) is expected.
+     *
+     * Using the following option the preferred axis-order can be indicated.
+     *
+     * @var string
+     */
+    const AXIS_ORDER_OPTION = 'axis-order=long-lat';
+
+    /**
+     * Optionally a SRID can be set to be used for Geographic types.
+     *
+     * @var int|null
+     */
+    public static $srid;
+
+    /**
+     * Gets the SQL declaration snippet for a field of this type.
+     *
+     * @param array $fieldDeclaration
+     *
+     * @return string
+     */
+    public function getSQLDeclaration(array $fieldDeclaration)
+    {
+        if ($fieldDeclaration['type']->getSQLType() === GeographyInterface::GEOGRAPHY) {
+            return 'GEOMETRY';
+        }
+
+        return strtoupper($fieldDeclaration['type']->getSQLType());
+    }
+
+    /**
+     * @param AbstractSpatialType $type
+     * @param string              $sqlExpr
+     *
+     * @return string
+     */
+    public function convertToPHPValueSQL(AbstractSpatialType $type, $sqlExpr)
+    {
+        return $type instanceof GeographyType
+            ? sprintf('ST_AsBinary(%s, "%s")', $sqlExpr, self::AXIS_ORDER_OPTION)
+            : sprintf('ST_AsBinary(%s)', $sqlExpr);
+    }
+
+    /**
+     * @param AbstractSpatialType $type
+     * @param string              $sqlExpr
+     *
+     * @return string
+     */
+    public function convertToDatabaseValueSQL(AbstractSpatialType $type, $sqlExpr)
+    {
+        return $type instanceof GeographyType && is_int(self::$srid)
+            ? sprintf('ST_GeomFromText(%s, %d)', $sqlExpr, self::$srid)
+            : sprintf('ST_GeomFromText(%s)', $sqlExpr);
+    }
+
+}
